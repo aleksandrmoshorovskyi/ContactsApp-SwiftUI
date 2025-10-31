8cfe83927f062dbcc49f6d0c62209e37f761c7d0
Oleksandr Moshorovskyi <46052383+aleksandrmoshorovskyi@users.noreply.github.com>
2025-10-31 13:07:28 +0200

    task_01and08

---
+++ ContactsApp-SwiftUI/ContactsApp-SwiftUI/ContactsApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsApp_SwiftUIApp.swift
+//  ContactsApp-SwiftUI
+//
+//  Created by Aleksandr Moroshovskyi on 31.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct ContactsApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
